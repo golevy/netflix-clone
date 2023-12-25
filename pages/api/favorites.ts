ff62ec137d73b69f867a56dea8445391d4583a72
Levy <golvwei@gmail.com>
2023-12-25 23:52:03 +0800

    Feature-011: Create User Favorite Movies Api

---
+++ pages/api/favorites.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { without } from "lodash"
+import prismadb from "~/lib/prismadb"
+import serverAuth from "~/lib/serverAuth"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end()
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req)
+
+    const favoriteMovies = await prismadb.movie.findMany({
+      where: {
+        id: {
+          in: currentUser?.favoriteIds,
+        },
+      },
+    })
+
+    return res.status(200).json(favoriteMovies)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ Error: "Bad Request" })
+  }
+}
