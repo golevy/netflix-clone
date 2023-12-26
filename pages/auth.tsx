ac9ef80da5f3d8c0cab9368b7411fc2ebe2b227b
Levy <golvwei@gmail.com>
2023-12-26 22:40:32 +0800

    Feature-013: Meta Info

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,7 @@ import Input from "~/components/Input"
 import { signIn } from "next-auth/react"
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
+import Head from "next/head"
 
 const Auth = () => {
   const [email, setEmail] = useState("")
@@ -50,77 +51,86 @@ const Auth = () => {
   }, [email, name, password, login])
 
   return (
-    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="Logo" className="h-12" />
-        </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign in" : "Register"}
-            </h2>
-            <div className="flex flex-col gap-4">
-              {variant === "register" && (
+    <>
+      <Head>
+        <title>Login | Register</title>
+        <meta name="description" content="Login | Register" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="bg-black w-full h-full lg:bg-opacity-50">
+          <nav className="px-12 py-5">
+            <img src="/images/logo.png" alt="Logo" className="h-12" />
+          </nav>
+          <div className="flex justify-center">
+            <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+              <h2 className="text-white text-4xl mb-8 font-semibold">
+                {variant === "login" ? "Sign in" : "Register"}
+              </h2>
+              <div className="flex flex-col gap-4">
+                {variant === "register" && (
+                  <Input
+                    label="Username"
+                    id="name"
+                    value={name}
+                    onChange={(e: any) => setName(e.target.value)}
+                  />
+                )}
                 <Input
-                  label="Username"
-                  id="name"
-                  value={name}
-                  onChange={(e: any) => setName(e.target.value)}
+                  label="Email"
+                  id="email"
+                  value={email}
+                  onChange={(e: any) => setEmail(e.target.value)}
+                  type="email"
                 />
-              )}
-              <Input
-                label="Email"
-                id="email"
-                value={email}
-                onChange={(e: any) => setEmail(e.target.value)}
-                type="email"
-              />
-              <Input
-                label="Password"
-                id="password"
-                value={password}
-                onChange={(e: any) => setPassword(e.target.value)}
-                type="password"
-              />
-            </div>
-            <button
-              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
-              onClick={variant === "login" ? login : register}
-            >
-              {variant === "login" ? "Login" : "Sign up"}
-            </button>
-            <div className="flex items-center gap-4 mt-8 justify-center">
-              {isLoginWithGoogleEnabled && (
+                <Input
+                  label="Password"
+                  id="password"
+                  value={password}
+                  onChange={(e: any) => setPassword(e.target.value)}
+                  type="password"
+                />
+              </div>
+              <button
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+                onClick={variant === "login" ? login : register}
+              >
+                {variant === "login" ? "Login" : "Sign up"}
+              </button>
+              <div className="flex items-center gap-4 mt-8 justify-center">
+                {isLoginWithGoogleEnabled && (
+                  <div
+                    onClick={() =>
+                      signIn("google", { callbackUrl: "/profiles" })
+                    }
+                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                  >
+                    <FcGoogle size={30} />
+                  </div>
+                )}
                 <div
-                  onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                  onClick={() => signIn("github", { callbackUrl: "/profiles" })}
                   className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
                 >
-                  <FcGoogle size={30} />
+                  <FaGithub size={30} />
                 </div>
-              )}
-              <div
-                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
-                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
-              >
-                <FaGithub size={30} />
               </div>
+              <p className="text-neutral-500 mt-12">
+                {variant === "login"
+                  ? "First time using Netflix?"
+                  : "Already have an account?"}
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  {variant === "register" ? "Login" : "Create an account"}
+                </span>
+              </p>
             </div>
-            <p className="text-neutral-500 mt-12">
-              {variant === "login"
-                ? "First time using Netflix?"
-                : "Already have an account?"}
-              <span
-                onClick={toggleVariant}
-                className="text-white ml-1 hover:underline cursor-pointer"
-              >
-                {variant === "register" ? "Login" : "Create an account"}
-              </span>
-            </p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from "next"
 import { getSession } from "next-auth/react"
+import Head from "next/head"
 import { useRouter } from "next/router"
 import useCurrentUser from "~/hooks/useCurrentUser"
 
@@ -25,29 +26,36 @@ const profiles = () => {
   const { data: user } = useCurrentUser()
 
   return (
-    <div className="flex items-center h-screen justify-center">
-      <div className="flex flex-col">
-        <h1 className="text-3xl md:text-6xl text-white text-center">
-          Who is watching?
-        </h1>
-        <div className="flex items-center justify-center gap-8 mt-10">
-          <div
-            onClick={() => {
-              router.push("/")
-            }}
-          >
-            <div className="group w-44 mx-auto">
-              <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
-                <img src="/images/default-blue.png" alt="" />
-              </div>
-              <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
-                {user?.name}
+    <>
+      <Head>
+        <title>Profile</title>
+        <meta name="description" content="Profile" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex items-center h-screen justify-center">
+        <div className="flex flex-col">
+          <h1 className="text-3xl md:text-6xl text-white text-center">
+            Who is watching?
+          </h1>
+          <div className="flex items-center justify-center gap-8 mt-10">
+            <div
+              onClick={() => {
+                router.push("/")
+              }}
+            >
+              <div className="group w-44 mx-auto">
+                <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                  <img src="/images/default-blue.png" alt="" />
+                </div>
+                <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+                  {user?.name}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
